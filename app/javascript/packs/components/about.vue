634cdf95f0554edbd6ce331b65199c5f9f81224a
reginad1 <reginadeangelis91@gmail.com>
2021-08-09 21:08:30 -0500

    about and booking info

--- app/javascript/packs/components/about.vue
+++ app/javascript/packs/components/about.vue
@@ -1,21 +1,31 @@
 <template>
   <div>
-  <v-card-title class="text-h5">
-            An awesome title
-  </v-card-title>
-  <v-card-text>
-    <p>
-      Duis lobortis massa imperdiet quam. Donec vitae orci sed dolor rutrum auctor. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Praesent congue erat at massa.
-    </p>
-
-    <p>
-      Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Etiam sit amet orci eget eros faucibus tincidunt. Donec sodales sagittis magna.
-    </p>
-
-    <p class="mb-0">
-      Ut leo. Suspendisse potenti. Duis vel nibh at velit scelerisque suscipit. Fusce pharetra convallis urna.
-    </p>
-  </v-card-text>
+    <v-card-title class="text-h5">
+      About Us
+    </v-card-title>
+    <v-card-text>
+      <p>
+        Fully customizable in studio convention! Innovative Fusion is made up of 12 faculty members that are some of the industry’s top working professionals. Each studio will have the opportunity to choose 4 of our outstanding faculty members, pick a 2 or 3 day event, tell us the amount of students attending, then we will calculate your total cost per dancer and provide a fully customized weekend just for your studio
+      </p><br>
+      <p>
+        We handle everything! You pick a weekend that works best for your studio, choose your faculty, then we book the faculty, hotels and transportation so all you have to do is sit back and relax while enjoying this experience for you and your students!
+      </p><br>
+      <p>
+        We will provide your students with the best training, motivation, education and care all in your studio. All of the Innovative Fusion faculty is dedicated to bringing the very best to your students in a positive, inspirational and educational manner. This opportunity provides your students training to help them excel to the next level, and learn from our industry experts all in the comfort of your own studio!
+      </p><br>
+      <p>
+        Innovative fusion is excited to offer a variety of classes! Ballet, Jazz, Jazz funk, Hip Hop, Contemporary, Lyrical, Tap, Musical Theater, Ballroom, Acro, Stretch, Improv, Heels, Technique, On Camera Training, Musicality for Dancers as well as the option to have a LIVE VOCALIST in your contemporary or lyrical class! 
+      </p><br>
+      <p>
+        In addition to the convention classes, we will also offer private lessons, competition routine critiques and choreography for an additional cost.
+      </p><br>
+      <p>
+        At the end of each event, we will allow time for photos with the faculty.
+      </p><br>
+      <p>
+        Contact us today to bring this experience to your studio
+      </p>
+    </v-card-text>
   </div>
 </template>
   
---
+++ app/javascript/packs/components/booking_info.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-card-title class="text-h5">
+      Booking Info
+    </v-card-title>
+    <v-card-text>
+      <p>
+        Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Sed hendrerit. Maecenas malesuada. Vestibulum ullamcorper mauris at ligula. Proin faucibus arcu quis ante.
+      </p>
+
+      <p class="mb-0">
+        Etiam vitae tortor. Curabitur ullamcorper ultricies nisi. Sed magna purus, fermentum eu, tincidunt eu, varius ut, felis. Aliquam lobortis. Suspendisse potenti.
+      </p>
+    </v-card-text>
+  </div>
+</template>
